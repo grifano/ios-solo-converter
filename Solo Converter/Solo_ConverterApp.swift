22ec08001c3e219d45d7630d49057c5ad00958f6
Serhii Orlenko <146512960+grifano@users.noreply.github.com>
2024-11-13 07:39:20 +0100

    Initial Commit

---
+++ Solo Converter/Solo_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Solo_ConverterApp.swift
+//  Solo Converter
+//
+//  Created by Serhii Orlenko on 13/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Solo_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
